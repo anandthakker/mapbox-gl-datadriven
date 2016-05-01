d014a85225f54b2a44b9e270eb2c85bdf49257a4
Anand Thakker <vestibule@anandthakker.net>
2016-05-01 12:42:35 -0400

    Add relative scale option

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var d3array = require('d3-array')
+
 module.exports = datadriven
 
 /**
@@ -18,28 +20,19 @@ module.exports = datadriven
  * @param {object} [options.paint] Common paint properties
  */
 function datadriven (map, options) {
+  var source = options.source
+  var styleFunction = options.styleFunction
+  var stopDataValues = styleFunction.stops.map(function (s) { return s[0] })
   ensureStyle(map, function () {
-    var source = options.source
     if (typeof source === 'object') {
       source = unique(map, options.prefix)
       map.addSource(source, options.source)
     }
 
-    var styleFunction = options.styleFunction
     styleFunction.stops.forEach(function (stop, i) {
-      var dataValue = stop[0]
       var styleValue = stop[1]
-
       var paint = {}
       paint[options.styleProperty] = styleValue
-
-      var filter = [ 'all' ]
-      filter.push([ '>=', styleFunction.property, dataValue ])
-      if (i < styleFunction.stops.length - 1) {
-        filter.push([ '<', styleFunction.property, styleFunction.stops[i + 1][0] ])
-      }
-      if (options.filter) { filter.push(options.filter) }
-
       map.addLayer({
         id: source + '-' + i,
         source: source,
@@ -47,10 +40,66 @@ function datadriven (map, options) {
         type: 'fill',
         layout: Object.assign({}, options.layout),
         paint: Object.assign(paint, options.paint),
-        filter: filter
+        filter: getFilter(i)
       })
     })
+
+    // add a dummy layer that ensures the source data gets loaded
+    map.addLayer({
+      id: source + '-dummy',
+      source: source,
+      'source-layer': options['source-layer'],
+      type: 'fill',
+      layout: {},
+      paint: { 'fill-color': '#000' },
+      filter: [ 'all', [ '==', '$type', 'Polygon' ], [ '==', '$type', 'Point' ] ]
+    })
+
+    if (styleFunction.type === 'relative') {
+      map.on('moveend', function () { onDataReady(updateScale) })
+      onDataReady(updateScale)
+    }
   })
+
+  function updateScale () {
+    var data = map.querySourceFeatures(source, {
+      sourceLayer: options['source-layer'],
+      filter: options.filter
+    })
+    .map(function (feat) { return feat.properties[styleFunction.property] })
+    .sort()
+
+    stopDataValues.map(function (q, i) {
+      var value = d3array.quantile(data, q)
+      styleFunction.stops[i][0] = value
+    })
+
+    stopDataValues.forEach(function (_, i) {
+      map.setFilter(source + '-' + i, getFilter(i))
+    })
+
+    console.log('updated dds', styleFunction)
+  }
+
+  function getFilter (i) {
+    var filter = [ 'all' ]
+    filter.push([ '>=', styleFunction.property, styleFunction.stops[i][0] ])
+    if (i < styleFunction.stops.length - 1) {
+      filter.push([ '<', styleFunction.property, styleFunction.stops[i + 1][0] ])
+    }
+    if (options.filter) { filter.push(options.filter) }
+    return filter
+  }
+
+  function onDataReady (cb) {
+    function onData () {
+      if (!map.loaded()) { return }
+      map.off('render', onData)
+      cb()
+    }
+    map.on('render', onData)
+    onData()
+  }
 }
 
 function unique (map, prefix) {
